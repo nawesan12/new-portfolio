58ba1b2b276e98aea115121c28880de666776794
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-04-30 01:38:49 -0300

    lenis

--- src/lenis.ts
+++ src/lenis.ts
@@ -3,10 +3,6 @@ import Lenis from '@studio-freight/lenis'
 
 const lenis = new Lenis()
 
-lenis.on('scroll', (e: any) => {
-  console.log(e)
-})
-
 export function raf(time: number) {
   lenis.raf(time)
   requestAnimationFrame(raf)
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import './style.css'
 import { raf } from "./lenis"
 
 // Query Selection
-// const $ = (selector: string) => document.querySelector(selector)
+const actualYearElement: HTMLElement | null = document.getElementById("actual-year")
 
-
+//@ts-ignore
+actualYearElement.innerText = String(new Date().getFullYear())
 
 // Initializing Lenis
 requestAnimationFrame(raf)
